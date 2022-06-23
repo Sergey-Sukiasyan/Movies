9fb7661c9a56783810648d82a9fd5fbc0e8024bb
Sergey Sukiasyan <sergeysukiasyan@gmail.com>
2022-06-23 20:05:03 +0400

    add files

--- server/build/index.js
+++ server/build/index.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./config/app");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+//Cors
+require("./config/cors");
 // Routes
 require("./routes/index");
-app_1.app.listen(5000, () => {
-    console.log('Server is Running!!!');
+const PORT = process.env.PORT || 5000;
+app_1.app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} port !!!`);
 });
